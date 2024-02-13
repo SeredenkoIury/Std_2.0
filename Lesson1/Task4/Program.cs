b935cee71225ac8a3b645a8a689de36a2a007d17
Iury Seredenko <iury.seredenko@gmail.com>
2024-02-13 23:38:03 +0300

    a b c d e max is

---
+++ Lesson1/Task4/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+
+int a = 6;
+int b = 79;
+int c = 9;
+int d = 42;
+int e = 130;
+int max = a;
+
+if ( b > max);
+{
+    max = b;
+}
+if (c > max);
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+
+Console.WriteLine("max is " + max);
