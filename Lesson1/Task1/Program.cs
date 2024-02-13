b439010729d455f4265ffb3687ab478078c4112b
Iury Seredenko <iury.seredenko@gmail.com>
2024-02-13 22:13:45 +0300

    added some variables lesson 1 - task 1

--- Lesson1/Task1/Program.cs
+++ Lesson1/Task1/Program.cs
@@ -2,7 +2,14 @@
 
 //Console.WriteLine("Hello world");
 
-int value = 100;
+int value = 100; // usual numbers storage type
+double d = 5.55; //double (1.1;2.53;...) storage type
+char c = '!';  // cymbol storage type
+bool b = true; // true or falce only
+
 Console.WriteLine(value);
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
 
 
