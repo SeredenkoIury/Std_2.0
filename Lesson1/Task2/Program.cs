a2bc2434b674335493a3cfb845b0c5749b6f974e
Iury Seredenko <iury.seredenko@gmail.com>
2024-02-13 22:50:37 +0300

    lesson1 task2 int long float double

--- Lesson1/Task2/Program.cs
+++ Lesson1/Task2/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+int i1 = 5;
+int i2 = 6;
+
+int sum1 = i1 + i2;
+Console.WriteLine(sum1);
+
+long l1 = 8;
+long sum2 = l1 + i1;
+Console.WriteLine(sum2);
+
+double d1 = 5.5;
+double sum3 = d1 + i1;
+Console.WriteLine(sum3);
+
+int product1 = i1 * i2;
+long product2 = l1 * i1;
+double product3 = d1 * i1;
+
+Console.WriteLine(product1);
+Console.WriteLine(product2);
+Console.WriteLine(product3);
+
