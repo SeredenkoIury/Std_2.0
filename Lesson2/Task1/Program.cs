73e4c1773c262805999133fc205340f006e7bd17
Iury Seredenko <iury.seredenko@gmail.com>
2024-02-20 21:36:04 +0300

    Added array info task1  lesson2

--- Lesson2/Task1/Program.cs
+++ Lesson2/Task1/Program.cs
@@ -1,3 +1,22 @@
 ﻿Console.Clear();
 Console.WriteLine("Lesson 1, Task 1");
 Console.WriteLine("");
+
+int[] arr = new int[5]; // Создали массив из целых чисел из 5 жлементов с именем arr
+arr[0] = 5;
+arr[1] = 7;
+arr[2] = 5;
+arr[3] = 4;
+arr[4] = 3;
+Console.WriteLine(arr[0]);
+Console.WriteLine(arr[1]);
+Console.WriteLine(arr[2]);
+Console.WriteLine(arr[3]);
+Console.WriteLine(arr[4]);
+
+//Console.WriteLine(arr[5]); выход за границы массива
+
+int[] arr2 = new int[5] { 1, 2, 3, 4, 5 };
+
+int[] arr3 = { 1, 2, 3, 4, 5 };
+
