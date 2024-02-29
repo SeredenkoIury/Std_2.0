388b99d5ce795bcfcd1eb25fb3d94284ce767144
Iury Seredenko <iury.seredenko@gmail.com>
2024-02-29 19:53:46 +0300

    Some fixes of Lesson 4 Dual Mass

--- Lesson4_DuoMass/Program.cs
+++ Lesson4_DuoMass/Program.cs
@@ -20,39 +20,39 @@ Console.WriteLine("");
 // Необходимо создать двумерный массив с размерами 3 х 5, состоящий
 // из целых чисел и вывести его на экран
 
-int[,] CreateMatrix(int rowCount, int columsCount)
-{
-    int[,] matrix = new int[rowCount, columsCount];
-    Random rnd = new Random();
+// int[,] CreateMatrix(int rowCount, int columsCount)
+// {
+//     int[,] matrix = new int[rowCount, columsCount];
+//     Random rnd = new Random();
 
-    for (int i = 0; i < matrix.GetLength(0); i = i + 1) // заполнение массива
-    {
+//     for (int i = 0; i < matrix.GetLength(0); i = i + 1) // заполнение массива
+//     {
 
-        for (int j = 0; j < matrix.GetLength(1); j = j + 1)
-        {
-            matrix[i, j] = rnd.Next(1, 11); // рещение преподавателя + вывод во вне создание рандома
-            //matrix[i,j] = new Random().Next(50,100); //   мое решение
-        }
-    }
-    return matrix;
-}
+//         for (int j = 0; j < matrix.GetLength(1); j = j + 1)
+//         {
+//             matrix[i, j] = rnd.Next(1, 11); // рещение преподавателя + вывод во вне создание рандома
+//             //matrix[i,j] = new Random().Next(50,100); //   мое решение
+//         }
+//     }
+//     return matrix;
+// }
 
-void ShowMatrix(int[,] matrix)
-{
+// void ShowMatrix(int[,] matrix)
+// {
 
-    for (int i = 0; i < matrix.GetLength(0); i = i + 1) // вывод двумерного массива 
-    {
+//     for (int i = 0; i < matrix.GetLength(0); i = i + 1) // вывод двумерного массива 
+//     {
 
-        for (int j = 0; j < matrix.GetLength(1); j = j + 1)
-        {
-            Console.Write($"{matrix[i, j]}\t");// интерполяция; \t это табуляция чтобы ровно все было
-        }
-        Console.WriteLine(" ");//перенос на новую строку чтобы была матрица 
-    }
-}
+//         for (int j = 0; j < matrix.GetLength(1); j = j + 1)
+//         {
+//             Console.Write($"{matrix[i, j]}\t");// интерполяция; \t это табуляция чтобы ровно все было
+//         }
+//         Console.WriteLine(" ");//перенос на новую строку чтобы была матрица 
+//     }
+// }
 
-int[,] matrix = CreateMatrix(4,5);
-ShowMatrix(matrix);
+// int[,] matrix = CreateMatrix(4,5);
+// ShowMatrix(matrix);
 
 
 //=================================================================================== Task 01
