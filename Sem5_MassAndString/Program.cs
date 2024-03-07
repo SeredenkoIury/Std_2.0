a605d1bb3def1c8edb0156b00f8b55e0c718b739
Iury Seredenko <iury.seredenko@gmail.com>
2024-03-07 19:41:34 +0300

    added Seminar 5 (Sem6) Massives (arrays) and strings. Work in need

---
+++ Sem5_MassAndString/Program.cs
@@ -0,0 +1,124 @@
+﻿Console.Clear();
+Console.WriteLine("Sem6, Mass and string");
+Console.WriteLine("");
+
+//=============================================================================================Theory
+// // Задайте массив символов (тип char []). Создайте строку из
+// // символов этого массива.
+
+// char[] array = new char{'t', '4', '#', 'y', '9'};
+
+// string str ="";
+
+// // for (int i = 0; i < array.Length; i++)
+// // {
+// //     str = str + array[i];
+// // }
+// // System.Console.WriteLine(str);
+
+// foreach (char item in array)
+// {
+//     str = str + item;
+// }
+
+// System.Console.WriteLine(str);
+//=============================================================================================Theory
+
+//============================================================================================= Task 1
+// На основе символов строки стринг сформируйте массив 
+//символов Чар и выведите массив на экран
+// string str = Console.ReadLine();
+// char[] array = new char[str.Length]; // берем длинну строчки
+
+// for (int i = 0; i < array.Length; i++)
+// {
+//     array[i] = str[i];
+//     System.Console.Write(array[i] + " ");
+// }
+//============================================================================================= Task 1
+
+//============================================================================================= Task 3
+// На основе символов строки стринг сформируйте массив 
+//символов Чар и выведите массив на экран
+// Выяснить сколько введенных букв гласных
+
+// Console.WriteLine("Add a String");
+// string userInput = Console.ReadLine();
+// char[] array = new char[userInput.Length]; // берем длинну строчки
+
+// string vowels = "aeiuo" ; // строчка элементы
+// int count = 0;
+
+// foreach (var item in userInput)
+// {
+//     if(vowels.Contains(item)) // Contains - есть ли 
+//     {
+//         count = count + 1;
+//     }
+// }
+
+// for (int i = 0; i < array.Length; i++)
+// {
+//     array[i] = userInput[i];
+//     System.Console.Write(array[i] + " ");
+// }
+
+// Console.WriteLine("aeiou is " + count);
+//============================================================================================= Task 3
+
+// Задайте массив символов (тип char []). Создайте строку из
+// символов этого массива.
+
+// char[] array = new char[]{'t', '4', '#', 'y', '9'};
+
+// string str ="";
+
+// // for (int i = 0; i < array.Length; i++)
+// // {
+// //     str = str + array[i];
+// // }
+// // System.Console.WriteLine(str);
+
+// foreach (char item in array)
+// {
+//     str = str + item;
+// }
+
+// System.Console.WriteLine(str);
+
+//==============================
+// // Задайте массив символов (тип char []). Создайте строку из
+// // символов этого массива.
+
+// char[] array = new char[]{'t', '4', '#', 'y', '9'};
+
+// string str ="";
+
+// // for (int i = 0; i < array.Length; i++)
+// // {
+// //     str = str + array[i];
+// // }
+// // System.Console.WriteLine(str);
+
+// foreach (char item in array)
+// {
+//     str = str + item;
+// }
+
+// System.Console.WriteLine(str)
+
+
+//============================================================================================= Task 3
+
+// void PrintArray(int[] array)
+// {
+
+// }
+
+// string GetString() 
+// {
+//     string t=" ";
+//     return t;
+// }
+
+
