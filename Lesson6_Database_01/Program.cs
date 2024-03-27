d104860fd767bd2a230d414c8b414f2bf843885a
Iury Seredenko <iury.seredenko@gmail.com>
2024-03-27 06:11:26 +0300

    Lesson been finished

---
+++ Lesson6_Database_01/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+Console.WriteLine("Lesson 6, Database 01");
+Console.WriteLine("by Seredenko Iury Danilovich");
+Console.WriteLine("");
+
+//============================================================
